72536f9e7e7b11652e17b622a558adf191393c7f
ilya shevchenko <ilya9610@gmail.com>
2025-03-19 20:42:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ from elasticapm.contrib.flask import ElasticAPM
 
 app = Flask(__name__)
 
-app.config['ELASTIC_APM'] = {
-    'SERVICE_NAME': 'BackendService_Ilya',
-    'SECRET_TOKEN': (Config.SECRET_TOKEN),
-    'SERVER_URL': (Config.SERVER_URL),
-    'ENVIRONMENT': 'dev',
-    'DEBUG': True,
-}
+#app.config['ELASTIC_APM'] = {
+#   'SERVICE_NAME': 'BackendService_Ilya',
+#    'SECRET_TOKEN': (Config.SECRET_TOKEN),
+#   'SERVER_URL': (Config.SERVER_URL),
+#    'ENVIRONMENT': 'dev',
+#   'DEBUG': True,
+#}
 
-apm = ElasticAPM(app)
+#apm = ElasticAPM(app)
 
 # Configure session handling for user state management
 app.config["SESSION_PERMANENT"] = Config.SESSION_PERMANENT
